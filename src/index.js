59f8cbcbc90cf5aad99dc5c85542248d4df0a7c4
SpiceWeasel66 <nagengasteva@gmail.com>
2023-07-27 23:19:08 +0100

    JS functions after pushing Button
     button to change innerHTML of elements
    
    plus: added Celsius and Fahrenheit,

--- src/index.js
+++ src/index.js
@@ -1,28 +1,33 @@
-
 //
 //
 //
-
-
 
 //
 // City search Buttons
 //
 
-function cityInfo (cityInformation) 
-{
-console.log(cityInformation);
-console.log("🐈‍⬛");
-console.log("🐈‍");
+function cityInfo(cityInformation) {
+  console.log(cityInformation.data);
+  document.querySelector("#main-city").innerHTML = cityInformation.data.name;
+  document.querySelector("#weather-now").innerHTML =
+    cityInformation.data.weather[0].description;
+  document.querySelector("#feels").innerHTML = Math.round(
+    cityInformation.data.main.feels_like
+  );
+  document.querySelector("#humid").innerHTML =
+    cityInformation.data.main.humidity;
+  document.querySelector("#wind").innerHTML = cityInformation.data.wind.speed;
+  document.querySelector("#today-temperature").innerHTML = Math.round(
+    cityInformation.data.main.temp
+  );
+  //document.querySelector("#main-city").innerHTML = cityInformation.data.name;
 }
-
-
 
 function getInputCity(city) {
   let apikey = "a5c55c774ac8198c087358853c4a79a9";
   let units = "metric"; // or let units = "metric"
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apikey}`;
-   axios.get(url).then(cityInfo);
+  axios.get(url).then(cityInfo);
 }
 
 function getCurrentCity(currentCity) {
@@ -31,9 +36,8 @@ function getCurrentCity(currentCity) {
   let apikey = "a5c55c774ac8198c087358853c4a79a9";
   let units = "metric"; // or let units = "metric"
   let latlonUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apikey}`;
-    axios.get(latlonUrl).then(cityInfo);
+  axios.get(latlonUrl).then(cityInfo);
 }
-
 
 function citySubmit(event) {
   event.preventDefault();
@@ -44,7 +48,6 @@ function currentSubmit(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(getCurrentCity);
 }
-
 
 document.querySelector("#search-form").addEventListener("submit", citySubmit);
 document
