2b0c7e17ed6d55b6f1e686d5d9e71d8d5fd72960
SpiceWeasel66 <nagengasteva@gmail.com>
2023-07-27 22:36:47 +0100

    added JS city search Buttons

--- src/index.js
+++ src/index.js
@@ -1,14 +1,54 @@
-let apikey = "a5c55c774ac8198c087358853c4a79a9";
-let units  = "metric" // or let units = "metric" 
-let city = "London"
-let lat = 51.477928;
-let lon = -0.001545;
+
+//
+//
+//
 
 
 
-let latlonUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apikey}`;
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apikey}`;
- console.log(url)
- console.log(latlonUrl);
+//
+// City search Buttons
+//
 
-// axios.get(url).then(firstfunction);
+function cityInfo (cityInformation) 
+{
+console.log(cityInformation);
+console.log("🐈‍⬛");
+console.log("🐈‍");
+}
+
+
+
+function getInputCity(city) {
+  let apikey = "a5c55c774ac8198c087358853c4a79a9";
+  let units = "metric"; // or let units = "metric"
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apikey}`;
+   axios.get(url).then(cityInfo);
+}
+
+function getCurrentCity(currentCity) {
+  let lat = currentCity.coords.latitude;
+  let lon = currentCity.coords.longitude;
+  let apikey = "a5c55c774ac8198c087358853c4a79a9";
+  let units = "metric"; // or let units = "metric"
+  let latlonUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apikey}`;
+    axios.get(latlonUrl).then(cityInfo);
+}
+
+
+function citySubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  getInputCity(city);
+}
+function currentSubmit(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getCurrentCity);
+}
+
+
+document.querySelector("#search-form").addEventListener("submit", citySubmit);
+document
+  .querySelector("#current-search-button")
+  .addEventListener("click", currentSubmit);
+console.log("🐈‍⬛");
+console.log("🐈‍");
