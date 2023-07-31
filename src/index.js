4afdec3d5991890002f0d297815721497592b2f8
SpiceWeasel66 <nagengasteva@gmail.com>
2023-07-31 16:31:36 +0100

    update forecast Icons

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 //
 
 function updateTime(apiInfo) {
-  console.log(apiInfo);
   let months = [
     // "Jan",  "Feb",  "Mar",  "Apr",  "May",  "Jun",  "Jul",  "Aug",  "Sep",  "Oct",  "Nov",  "Dec",
     "January",
@@ -78,55 +77,16 @@ function changeIconColor(iconName) {
 }
 
 function changeIcon(iconName) {
-  let name = iconName;
-  let nameboth = name.substring(0, 2);
-  if (name === "clear-sky-day") {
-    document.querySelector("#iconT").innerHTML = "wb_sunny";
-  }
-  if (name === "clear-sky-night") {
-    document.querySelector("#iconT").innerHTML = " nightlight";
-  }
-  if (name === "few-clouds-day") {
-    document.querySelector("#iconT").innerHTML = "partly_cloudy_day";
-  }
+ let iconValue = iconMap[iconName];
+ let icon = document.querySelector("#iconT");
+ icon.innerHTML = iconValue;
 
-  if (name === "few-clouds-night") {
-    document.querySelector("#iconT").innerHTML = "partly_cloudy_night";
-  }
-
-  if (nameboth === "sca") {
-    document.querySelector("#iconT").innerHTML = "cloud";
-  }
-
-  if (name === "bro") {
-    document.querySelector("#iconT").innerHTML = " filter_drama";
-  }
-
-  if (name === "sho") {
-    document.querySelector("#iconT").innerHTML = "rainy";
-  }
-
-  if (name === "rai") {
-    document.querySelector("#iconT").innerHTML = "cloudy_snowing ";
-  }
-
-  if (name === "thu") {
-    document.querySelector("#iconT").innerHTML = " thunderstorm";
-  }
-
-  if (name === "sno") {
-    document.querySelector("#iconT").innerHTML = "weather_snowy ";
-  }
-
-  if (name === "mis") {
-    document.querySelector("#iconT").innerHTML = "foggy";
-  }
   changeIconColor(iconName);
 }
 
 //
 //
-//update forecast
+//update forecast days
 
 function getForecastData(city) {
   let apiKey = "5101b1tb3fba4e5cedfo0b346a6ccc32";
@@ -142,27 +102,95 @@ function forecastDays(timestamp) {
   return days[day];
 }
 
+//
+//update forecast icons
+
+let iconMap = {
+  "clear-sky-day": "wb_sunny",
+  "clear-sky-night": "nightlight",
+  "few-clouds-day": "partly_cloudy_day",
+  "few-clouds-night": "partly_cloudy_night",
+  "scattered-clouds-day": "cloud",
+  "scattered-clouds-night": "cloud",
+  "broken-clouds-day": "filter_drama",
+  "broken-clouds-night": "filter_drama",
+  "shower-rain-day": "rainy",
+  "shower-rain-night": "rainy",
+  "rain-day": "cloudy_snowing",
+  "rain-night": "cloudy_snowing",
+  "thunderstorm-day": "thunderstorm",
+  "thunderstorm-night": "thunderstorm",
+  "snow-day": "weather_snowy",
+  "snow-night": "weather_snowy",
+  "mist-day": "foggy",
+  "mist-night": "foggy",
+};
+
+function getHTMLIcon(iconValue) {
+  return iconMap[iconValue];
+}
+
 function displayForecast(apiInfo) {
   let forecastarray = apiInfo.data.daily;
   let forecastsection = document.querySelector("#forecast");
   let forecastHtml = "";
 
-  forecastarray.forEach(function (info, index) {
-    if (index < 4) {
-      forecastHtml =
-        forecastHtml +
-        `  <li> <div class="row">
-      <div class="col-6">${info.condition.icon}</div>
+  forecastHtml =
+    forecastHtml +
+    `  <li> <div class="row">
+      <div class="col-6"><span class="material-symbols-outlined">${getHTMLIcon(
+        forecastarray[0].condition.icon
+      )}</span></div>
       <div class="col-6">
-         ${forecastDays(info.time)}
+         ${forecastDays(forecastarray[0].time)}
         <br />
-        ${Math.round(info.temperature.maximum)} | ${Math.round(
-          info.temperature.minimum
-        )}  </div>  </div> </li> `;
-    }
-  });
+        ${Math.round(forecastarray[0].temperature.maximum)} | ${Math.round(
+      forecastarray[0].temperature.minimum
+    )}  </div>  </div> </li> ` +
+    `  <li> <div class="row">
+      <div class="col-6"><span class="material-symbols-outlined">${getHTMLIcon(
+        forecastarray[1].condition.icon
+      )}</span></div>
+      <div class="col-6">
+         ${forecastDays(forecastarray[1].time)}
+        <br />
+        ${Math.round(forecastarray[1].temperature.maximum)} | ${Math.round(
+      forecastarray[1].temperature.minimum
+    )}  </div>  </div> </li> ` +
+    `  <li> <div class="row">
+      <div class="col-6"><span class="material-symbols-outlined">${getHTMLIcon(
+        forecastarray[2].condition.icon
+      )}</span></div>
+      <div class="col-6">
+         ${forecastDays(forecastarray[2].time)}
+        <br />
+        ${Math.round(forecastarray[2].temperature.maximum)} | ${Math.round(
+      forecastarray[2].temperature.minimum
+    )}  </div>  </div> </li> ` +
+    `  <li> <div class="row">
+      <div class="col-6"><span class="material-symbols-outlined">${getHTMLIcon(
+        forecastarray[3].condition.icon
+      )}</span></div>
+      <div class="col-6">
+         ${forecastDays(forecastarray[3].time)}
+        <br />
+        ${Math.round(forecastarray[3].temperature.maximum)} | ${Math.round(
+      forecastarray[3].temperature.minimum
+    )}  </div>  </div> </li> ` +
+    `  <li> <div class="row">
+      <div class="col-6"><span class="material-symbols-outlined">${getHTMLIcon(
+        forecastarray[4].condition.icon
+      )}</span></div>
+      <div class="col-6">
+         ${forecastDays(forecastarray[4].time)}
+        <br />
+        ${Math.round(forecastarray[4].temperature.maximum)} | ${Math.round(
+      forecastarray[4].temperature.minimum
+    )}  </div>  </div> </li> `;
+
   forecastsection.innerHTML = forecastHtml;
 }
+
 //
 //
 // City search Buttons
@@ -218,11 +246,9 @@ function currentSubmit(event) {
   navigator.geolocation.getCurrentPosition(getCurrentCity);
 }
 
-
-//let celsiusTemperature = null;
-
 document.querySelector("#search-form").addEventListener("submit", citySubmit);
-document.querySelector("#current-search-button")
+document
+  .querySelector("#current-search-button")
   .addEventListener("click", currentSubmit);
 
 getInputCity("Sydney");
